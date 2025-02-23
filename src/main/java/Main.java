0b5f9720a18208985ce6f82449ee7ce25fd2fcd5
Yū <artembezzzz@gmail.com>
2025-02-24 00:42:44 +0100

    rewrote to use GUI onlu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,74 @@
 import Entities.BlackBeatEntry;
-import Entities.Human;
 import Entities.Init;
 import Entities.MLCEntry;
+import Entities.Person;
 import Parsers.*;
-import jakarta.persistence.EntityManager;
 
+import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            throw new RuntimeException("Usage program.jar datapath outpath");
-        }
+    private static File inputData;
 
+    public static void main(String[] args) {
         Init.setDB("Names");
         File f = new File("db.xlsx");
         if (f.exists()) {
             XLSXtoDB.load(f);
         }
-        ArrayList<BlackBeatEntry> sourceData = BlackBeatToList.parse(new File(args[0]));
+
+        try {
+            selectDataFile();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<BlackBeatEntry> sourceData = BlackBeatToList.parse(inputData);
         ArrayList<MLCEntry> outData = BlackBeatToMLC.convert(sourceData);
         String path = "new.xlsx";
-        if (args.length == 2) {
-            path = args[1];
-        }
-        MLCListToXLSX.record(new File(path),outData);
+        MLCListToXLSX.record(new File(path), outData);
 
-        if (f.exists()) if (!f.delete()) System.out.println("Couldn't delete db.xlsx");
+        if (f.exists()) if (!f.delete()) System.err.println("Couldn't delete db.xlsx");
 
         DBtoXLSX.write(f);
+    }
+
+    private static void selectDataFile() throws InterruptedException {
+        Runnable task = () -> {
+            File dir = new File("");
+            JFileChooser jfc = new JFileChooser(dir.getAbsolutePath());
+            JDialog dial = new JDialog();
+            dial.setTitle("Choose input data");
+            jfc.setDialogTitle("Choose input data");
+            Person.addListeners(dial);
+            dial.setContentPane(jfc);
+            dial.pack();
+            dial.setVisible(true);
+
+            Thread current = Thread.currentThread();
+
+            jfc.addActionListener(e -> {
+                if (e.getActionCommand().equals("ApproveSelection")) {
+                    inputData = jfc.getSelectedFile();
+                    synchronized (current) {
+                        current.interrupt();
+                    }
+                    dial.dispose();
+                } else if (e.getActionCommand().equals("CancelSelection")) {
+                    System.exit(1);
+                }
+            });
+
+            synchronized (current) {
+                try {
+                    current.wait();
+                } catch (InterruptedException _) {}
+            }
+        };
+
+        Thread t = new Thread(task);
+        t.start();
+        t.join();
     }
 }
